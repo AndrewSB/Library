cba71b55ac562942ce04e72e8003898ea8f72fac
Andrew Breckenridge <asbreckenridge@me.com>
2015-11-03 16:15:26 -0800

    add an Alert helper

---
+++ Library/Alert.swift
@@ -0,0 +1,20 @@
+//
+//  Alert.swift
+//  Library
+//
+//  Created by Andrew Breckenridge on 11/3/15.
+//  Copyright © 2015 Andrew Breckenridge. All rights reserved.
+//
+
+// Syntactic sugar for UIAlertController
+
+import UIKit
+
+extension UIAlertController {
+    
+    convenience init(error title: String = "Uh oh!", message: String) {
+        self.init(title: title, message: message, preferredStyle: .Alert)
+        self.addAction(UIAlertAction(title: "Ok", style: .Cancel, handler: nil))
+    }
+    
+}
