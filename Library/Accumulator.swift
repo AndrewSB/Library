1ea4eb0da11be75cea330260f5b6198efed2bcd4
Andrew Breckenridge <asbreckenridge@me.com>
2016-01-03 18:08:02 -0800

    version 0.1 😆

--- Library/Accumulator.swift
+++ Library/Accumulator.swift
@@ -6,9 +6,6 @@
 //  Copyright © 2015 Andrew Breckenridge. All rights reserved.
 //
 
-import Foundation
-import Library
-
 public func accumulator(initial: Int = -1, incrementBy: Int = 1) -> () -> Int {
     var value = initial
     return {
--- Library/Alert.swift
+++ Library/Alert.swift
@@ -6,13 +6,13 @@
 //  Copyright © 2015 Andrew Breckenridge. All rights reserved.
 //
 
-// Syntactic sugar for UIAlertController
+// Syntactic sugar for error messaging with UIAlertController
 
 import UIKit
 
 public extension UIAlertController {
     
-    public convenience init(title: String = "Uh oh!", errorMessage: String) {
+    public convenience init(error title: String = "Uh oh!", errorMessage: String) {
         self.init(title: title, message: errorMessage, preferredStyle: .Alert)
         self.addAction(UIAlertAction(title: "Ok", style: .Cancel, handler: nil))
     }
