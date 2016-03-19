e2c0ba8f05ee77d6436282ce16e48ecb7d232bf5
Andrew Breckenridge <asbreckenridge@me.com>
2016-03-19 15:22:19 -0700

    RoundedImageView -> RoundedView

---
+++ Library/UI/RoundedView.swift
@@ -0,0 +1,80 @@
+//
+//  RoundedImageView.swift
+//  Library
+//
+//  Created by Andrew Breckenridge on 3/19/16.
+//  Copyright © 2016 Andrew Breckenridge. All rights reserved.
+//
+
+import UIKit
+
+public class RoundedView: UIView {
+    
+    public enum RoundBy {
+        case Height
+        case Width
+    }
+    
+    /**
+     Whether the view should be use the height or width of the frame to round itself
+     */
+    public var roundBy: RoundBy = .Height
+
+    // TODO: @IBInspectable isn't preserved when the `Library` is imported as a framework. #pleasefixthisapple
+    /**
+     The corner radius to which the `UIView`'s layer is to be clipped
+     */
+    @IBInspectable public lazy var cornerRadius: CGFloat = (self.roundBy == .Height ? self.frame.height : self.frame.width) / 2
+
+    override public func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.layer.cornerRadius = cornerRadius
+        self.clipsToBounds = true
+    }
+
+}
+
+public class RoundedImageView: UIImageView {
+
+    /**
+     Whether the view should be use the height or width of the frame to round itself
+     */
+    public var roundBy: RoundedView.RoundBy = .Height
+    
+    // TODO: @IBInspectable isn't preserved when the `Library` is imported as a framework. #pleasefixthisapple
+    /**
+     The corner radius to which the `UIView`'s layer is to be clipped
+     */
+    @IBInspectable public lazy var cornerRadius: CGFloat = (self.roundBy == .Height ? self.frame.height : self.frame.width) / 2
+    
+    override public func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.layer.cornerRadius = cornerRadius
+        self.clipsToBounds = true
+    }
+    
+}
+
+public class RoundedButton: UIButton {
+    
+    /**
+     Whether the button should be use the height or width of the frame to round itself
+     */
+    public var roundBy: RoundedView.RoundBy = .Height
+    
+    // TODO: @IBInspectable isn't preserved when the `Library` is imported as a framework. #pleasefixthisapple
+    /**
+     The corner radius to which the `UIButton`'s layer is to be clipped
+     */
+    @IBInspectable public lazy var cornerRadius: CGFloat = (self.roundBy == .Height ? self.frame.height : self.frame.width) / 2
+    
+    override public func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.layer.cornerRadius = cornerRadius
+        self.clipsToBounds = true
+    }
+    
+}
