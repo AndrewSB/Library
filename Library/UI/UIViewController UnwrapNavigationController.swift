1edd31fa28e6c652ea19f30dee2f2029612f46d0
Andrew Breckenridge <asbreckenridge@me.com>
2016-05-02 12:42:08 -0700

    add UIViewController UnwrapNavigationController

---
+++ Library/UI/UIViewController UnwrapNavigationController.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController UnwrapNavigationController.swift
+//  Library
+//
+//  Created by Andrew Breckenridge on 5/2/16.
+//  Copyright © 2016 Andrew Breckenridge. All rights reserved.
+//
+
+import UIKit
+
+public extension UIViewController {
+    
+    public func unwrapNavCon() -> UIViewController {
+    
+        let unwrappedViewController = (self as? UINavigationController)?.viewControllers[0]
+        
+        return unwrappedViewController ?? self
+        
+    }
+    
+}
