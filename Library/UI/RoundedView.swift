e3e127cb6094325f02452e4ff90e211a4e02bda3
Andrew Breckenridge <asbreckenridge@me.com>
2016-05-24 15:21:56 -0700

    refactor all RoundedViews to use Roundable

--- Library/UI/RoundedView.swift
+++ Library/UI/RoundedView.swift
@@ -9,117 +9,52 @@
 #if os(iOS)
 import UIKit
 
-public class RoundedView: UIView {
-    
-    public enum RoundBy {
-        case Height
-        case Width
-    }
-    
-    /**
-     Whether the view should be use the height or width of the frame to round itself
-     */
-    public var roundBy: RoundBy = .Height
-
-    // TODO: @IBInspectable isn't preserved when the `Library` is imported as a framework. #pleasefixthisapple
-    /**
-     The corner radius to which the `UIView`'s layer is to be clipped
-     */
-    @IBInspectable public lazy var cornerRadius: CGFloat = (self.roundBy == .Height ? self.frame.height : self.frame.width) / 2
+public class RoundedView: UIView, Roundable {
 
     override public func awakeFromNib() {
         super.awakeFromNib()
         
-        self.layer.cornerRadius = cornerRadius
-        self.clipsToBounds = true
+        round()
     }
 
 }
 
-public class RoundedImageView: UIImageView {
+public class RoundedImageView: UIImageView, Roundable {
 
-    /**
-     Whether the view should be use the height or width of the frame to round itself
-     */
-    public var roundBy: RoundedView.RoundBy = .Height
-    
-    // TODO: @IBInspectable isn't preserved when the `Library` is imported as a framework. #pleasefixthisapple
-    /**
-     The corner radius to which the `UIView`'s layer is to be clipped
-     */
-    @IBInspectable public lazy var cornerRadius: CGFloat = (self.roundBy == .Height ? self.frame.height : self.frame.width) / 2
-    
     override public func awakeFromNib() {
         super.awakeFromNib()
         
-        self.layer.cornerRadius = cornerRadius
-        self.clipsToBounds = true
+        round()
     }
     
 }
 
-public class RoundedButton: UIButton {
-    
-    /**
-     Whether the button should be use the height or width of the frame to round itself
-     */
-    public var roundBy: RoundedView.RoundBy = .Height
-    
-    // TODO: @IBInspectable isn't preserved when the `Library` is imported as a framework. #pleasefixthisapple
-    /**
-     The corner radius to which the `UIButton`'s layer is to be clipped
-     */
-    @IBInspectable public lazy var cornerRadius: CGFloat = (self.roundBy == .Height ? self.frame.height : self.frame.width) / 2
+public class RoundedButton: UIButton, Roundable {
     
     override public func awakeFromNib() {
         super.awakeFromNib()
         
-        self.layer.cornerRadius = cornerRadius
-        self.clipsToBounds = true
+        round()
     }
     
 }
 
-public class RoundedLabel: UILabel {
-    
-    /**
-     Whether the label should be use the height or width of the frame to round itself
-     */
-    public var roundBy: RoundedView.RoundBy = .Height
-    
-    // TODO: @IBInspectable isn't preserved when the `Library` is imported as a framework. #pleasefixthisapple
-    /**
-    The corner radius to which the `UILabel`'s layer is to be clipped
-    */
-    @IBInspectable public lazy var cornerRadius: CGFloat = (self.roundBy == .Height ? self.frame.height : self.frame.width) / 2
+public class RoundedLabel: UILabel, Roundable {
     
     override public func awakeFromNib() {
         super.awakeFromNib()
         
-        self.layer.cornerRadius = cornerRadius
-        self.clipsToBounds = true
+        round()
     }
     
 }
 
-public class RoundedTextField: UITextField {
-    
-    /**
-     Whether the text field should be use the height or width of the frame to round itself
-     */
-    public var roundBy: RoundedView.RoundBy = .Height
-    
-    // TODO: @IBInspectable isn't preserved when the `Library` is imported as a framework. #pleasefixthisapple
-    /**
-    The corner radius to which the `UITextField`'s layer is to be clipped
-    */
-    @IBInspectable public lazy var cornerRadius: CGFloat = (self.roundBy == .Height ? self.frame.height : self.frame.width) / 2
+public class RoundedTextField: UITextField, Roundable {
     
     override public func awakeFromNib() {
         super.awakeFromNib()
         
-        self.layer.cornerRadius = cornerRadius
-        self.clipsToBounds = true
+        round()
     }
     
 }
