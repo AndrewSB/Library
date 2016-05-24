b622dabdd254e94d2a2c6b4f6c22637df88782b8
Andrew Breckenridge <asbreckenridge@me.com>
2016-05-24 15:20:15 -0700

    create Roundable protocol

---
+++ Library/UI/Roundable.swift
@@ -0,0 +1,38 @@
+//
+//  Roundable.swift
+//  Library
+//
+//  Created by Andrew Breckenridge on 5/24/16.
+//  Copyright © 2016 Andrew Breckenridge. All rights reserved.
+//
+
+import Foundation
+
+#if os(iOS)
+import UIKit
+#endif
+
+public enum RoundBy {
+    case Height
+    case Width
+}
+
+public protocol Roundable {
+    var roundBy: RoundBy { get }
+    func round()
+}
+
+#if os(iOS)
+public extension Roundable where Self: UIView {
+
+    var roundBy: RoundBy { return .Height }
+    
+    func round() {
+        let cornerRadius = (self.roundBy == .Height ? self.frame.height : self.frame.width) / 2
+        
+        self.layer.cornerRadius = cornerRadius
+        self.clipsToBounds = true
+    }
+    
+}
+#endif
