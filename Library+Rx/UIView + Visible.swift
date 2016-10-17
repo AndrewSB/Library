a66cb399ff1b86dba4962b52858172a8630604d3
Andrew Breckenridge <asbreckenridge@me.com>
2016-10-17 16:30:21 -0700

    create UIView + Visible

---
+++ Library+Rx/UIView + Visible.swift
@@ -0,0 +1,16 @@
+import UIKit
+import RxSwift
+import RxCocoa
+
+public extension Reactive where Base: UIView {
+    
+    /**
+     Bindable sink for `visible` property.
+     */
+    public var visible: UIBindingObserver<Base, Bool> {
+        return UIBindingObserver(UIElement: self.base) { view, visible in
+            view.isHidden = !visible
+        }
+    }
+    
+}
