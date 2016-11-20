c92ab0f5f80308eda7a005655db6cd93f9a56683
Andrew Breckenridge <asbreckenridge@me.com>
2016-11-19 17:50:01 -0800

    add Dictionary Operator+.swift

---
+++ Library/Util/Dictionary Operator+.swift
@@ -0,0 +1,9 @@
+//
+//  Dictionary Operator+.swift
+//  Library
+//
+//  Created by Andrew Breckenridge on 11/19/16.
+//  Copyright © 2016 Andrew Breckenridge. All rights reserved.
+//
+
+import Foundation
