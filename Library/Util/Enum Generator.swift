0e58692d3ab5e9eb2fe446980ec90d7b532a2ca2
Andrew Breckenridge <asbreckenridge@me.com>
2016-11-19 17:50:20 -0800

    implement enum generator in swift3

--- Library/Util/Enum Generator.swift
+++ Library/Util/Enum Generator.swift
@@ -8,8 +8,25 @@
 
 import Foundation
 
-// Needs to be rewritten for Swift 3
-#if !swift(>=3)
+#if swift(>=3)
+public protocol EnumCollection: Hashable {}
+extension EnumCollection {
+    static func cases() -> AnySequence<Self> {
+        typealias S = Self
+        return AnySequence { () -> AnyIterator<S> in
+            var raw = 0
+            return AnyIterator {
+                let current: Self = withUnsafePointer(to: &raw) {
+                    $0.withMemoryRebound(to: S.self, capacity: 1) { $0.pointee }
+                }
+                guard current.hashValue == raw else { return nil }
+                raw += 1
+                return current
+            }
+        }
+    }
+}
+#else
 
 public protocol EnumCollection : Hashable {}
 public extension EnumCollection {
