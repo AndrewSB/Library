03483108e04bc28e378ce399fd17a46083a0b3bc
Andrew Breckenridge <asbreckenridge@me.com>
2016-12-02 11:44:08 -0800

    implement Array + Last Occourance

--- Library/Util/Array + Last Occurrence.swift
+++ Library/Util/Array + Last Occurrence.swift
@@ -2,8 +2,24 @@ import Foundation
 
 public extension Array where Element: Equatable {
 
-    // swiftlint:disable:next variable_name (of)
-    func lastIndex(of: Element) -> Index? {
+    /**
+     The last index of an array is an effecient computation of the array.reversed.index(of: ). 
+     Runtime is O(i), instead of O(N+i), where i is the index of first matched element reversed.
+    
+     :param: of the Equatable element you'd like the index of
+
+     :returns: The first index from the end of the array if it exists, else nil
+     */
+    func lastIndex(of match: Element) -> Index? { // swiftlint:disable variable_name (of)
+        for idx in 0..<self.endIndex {
+            let reversedIdx = (self.endIndex - 1) - idx
+            let candidate = self[reversedIdx]
+
+            if candidate == match {
+                return reversedIdx
+            }
+        }
+
         return nil
     }
 
